c94e175285f5b06cd7e40f04933dbbc72c4ceb1f
cc_ed <68816862+cocobun@users.noreply.github.com>
2022-10-03 15:54:38 +0800

    Update test1.py

--- test1.py
+++ test1.py
@@ -1,1 +1,15 @@
+import streamlit as st
+import pandas as pd
+import numpy as np
 
+st.title("my sexy little")
+
+
+date_column="date/time"
+
+@st.cache #to increase speed of app (decorator)
+def get data():
+  data=pd.read_csv
+  
+  
+data=get_data()
